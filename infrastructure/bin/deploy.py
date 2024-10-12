f4c9b0c7dcad19dd2e433ca6f4868826d72666fb
Nelson Almeida <nelson.martinsalmeidaa@gmail.com>
2024-10-12 17:56:43 +0100

    Docker build, deployment of the ecs, ecr and vpcs for the authorization service
    Signed-off-by: Nelson Almeida <nelson.martinsalmeidaa@gmail.com>

--- infrastructure/bin/deploy.py
+++ infrastructure/bin/deploy.py
@@ -1,7 +1,7 @@
-import subprocess
-from pathlib import Path
 import argparse
 import logging
+import subprocess
+from pathlib import Path
 
 # Configure logging
 logging.basicConfig(
@@ -10,48 +10,69 @@ logging.basicConfig(
 )
 LOG = logging.getLogger(__name__)
 
+
 # Terraform init
 def terraform_init(module_path: Path):
     subprocess.run(["terraform", "init"], cwd=module_path, check=True)
     LOG.info("Terraform init completed.")
+
 
 # Terraform plan
 def terraform_plan(module_path: Path):
     subprocess.run(["terraform", "plan"], cwd=module_path, check=True)
     LOG.info("Terraform plan completed.")
 
+
 # Terraform apply
 def terraform_apply(module_path: Path):
     subprocess.run(["terraform", "apply", "-auto-approve"], cwd=module_path, check=True)
     LOG.info("Terraform apply completed.")
 
+
 # Create Docker image for the API
 def create_docker_image(docker_file_path: Path, module_path: Path, image_name: str):
     subprocess.run(
-        f"docker build --ssh default --platform linux/x86_64 --no-cache --pull {module_path} --progress=plain -f {docker_file_path} -t {image_name} ;",
+        [
+            "docker",
+            "build",
+            "--ssh",
+            "default",
+            "--platform",
+            "linux/x86_64",
+            "--no-cache",
+            "--pull",
+            str(module_path),
+            "--progress=plain",
+            "-f",
+            str(docker_file_path),
+            "-t",
+            image_name,
+        ],
         check=True,
-        shell=True,
     )
     LOG.info("Docker image created.")
+
 
 # Push Docker image to ECR
 def push_docker_image_to_ecr(image_name: str, ecr_repository: str, aws_region: str):
     subprocess.run(
-        f"aws ecr get-login-password --region {aws_region} | docker login --username AWS --password-stdin {ecr_repository}",
+        f"aws ecr get-login-password --region {aws_region} | "
+        f"docker login --username AWS --password-stdin {ecr_repository}",
         shell=True,
         check=True,
     )
     subprocess.run(
-        f"docker tag {image_name}:latest {ecr_repository}/{image_name}:latest",
+        f"docker tag {image_name}: latest {ecr_repository}/{image_name}: latest",
         shell=True,
         check=True,
     )
     subprocess.run(
-        f"docker push {ecr_repository}/{image_name}:latest",
+        f"docker push {ecr_repository}/{image_name}: latest",
         shell=True,
         check=True,
     )
     LOG.info("Docker image pushed to ECR.")
+
 
 # Deploy the API to ECS
 def deploy_to_ecs(cluster_name: str, service_name: str):
@@ -62,20 +83,69 @@ def deploy_to_ecs(cluster_name: str, service_name: str):
     )
     LOG.info("API deployed to ECS.")
 
+
+def terraform_destroy(module_path: Path):
+    subprocess.run(
+        ["terraform", "destroy", "-auto-approve"], cwd=module_path, check=True
+    )
+    LOG.info("Terraform destroy completed.")
+
+
 # Main function with argument parsing
 def main():
-    parser = argparse.ArgumentParser(description="Deploy the API using Terraform, Docker, and AWS ECS.")
+    parser = argparse.ArgumentParser(
+        description="Deploy the API using Terraform, Docker, and AWS ECS."
+    )
 
     # Arguments
-    parser.add_argument('--module-path', type=str, required=True, help="Path to the Terraform module.")
-    parser.add_argument('--dockerfile-path', type=str, required=True, help="Path to the Dockerfile.")
-    parser.add_argument('--ecr-repository', type=str, required=True, help="ECR repository URL.")
-    parser.add_argument('--aws-region', type=str, required=True, help="AWS region for ECR.")
-    parser.add_argument('--cluster-name', type=str, required=True, help="ECS cluster name.")
-    parser.add_argument('--service-name', type=str, required=True, help="ECS service name.")
-    parser.add_argument('--image-name', type=str, required=False, help="Docker image name.")
+    parser.add_argument(
+        "--module-path", type=str, required=True, help="Path to the Terraform module."
+    )
+    parser.add_argument(
+        "--dockerfile-path", type=str, required=True, help="Path to the Dockerfile."
+    )
+    parser.add_argument(
+        "--ecr-repository", type=str, required=True, help="ECR repository URL."
+    )
+    parser.add_argument(
+        "--aws-region", type=str, required=True, help="AWS region for ECR."
+    )
+    parser.add_argument(
+        "--cluster-name", type=str, required=True, help="ECS cluster name."
+    )
+    parser.add_argument(
+        "--service-name", type=str, required=True, help="ECS service name."
+    )
+    parser.add_argument(
+        "--image-name", type=str, required=False, help="Docker image name."
+    )
+    parser.add_argument(
+        "--run-type",
+        type=str,
+        required=False,
+        help="Type of run: 'full','tf_deploy','tf_destroy' or 'push_image.",
+    )
 
     args = parser.parse_args()
+
+    if args.run_type == "tf_deploy":
+        LOG.info("Deploying Terraform resources.")
+        terraform_init(Path(args.module_path))
+        terraform_plan(Path(args.module_path))
+        terraform_apply(Path(args.module_path))
+
+    if args.run_type == "push_image":
+        LOG.info("Building and pushing Docker image.")
+        create_docker_image(
+            Path(args.dockerfile_path), Path(args.module_path), args.image_name
+        )
+        push_docker_image_to_ecr(
+            "authorizationimage", args.ecr_repository, args.aws_region
+        )
+
+    if args.run_type == "tf_destroy":
+        LOG.info("Destroying Terraform resources.")
+        terraform_destroy(Path(args.module_path))
 
     # Run Terraform commands
     terraform_init(Path(args.module_path))
@@ -83,11 +153,14 @@ def main():
     terraform_apply(Path(args.module_path))
 
     # Build and push Docker image
-    create_docker_image(Path(args.dockerfile_path), Path(args.module_path), args.image_name)
+    create_docker_image(
+        Path(args.dockerfile_path), Path(args.module_path), args.image_name
+    )
     push_docker_image_to_ecr("authorizationimage", args.ecr_repository, args.aws_region)
 
     # Deploy to ECS
     deploy_to_ecs(args.cluster_name, args.service_name)
+
 
 if __name__ == "__main__":
     main()
--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -9,7 +9,7 @@ terraform {
 
 variable "AWS_REGION" {
   type    = string
-  default = "us-east-1"
+  default = "eu-west-1"  # Updated to match your region
 }
 
 variable "AWS_ACCESS_KEY_ID" {
@@ -23,10 +23,85 @@ variable "AWS_SECRET_ACCESS_KEY" {
 }
 
 provider "aws" {
-    region                      = var.AWS_REGION
-    access_key                  = var.AWS_ACCESS_KEY_ID
-    secret_key                  = var.AWS_SECRET_ACCESS_KEY
-    skip_metadata_api_check     = true
+  region                      = var.AWS_REGION
+  access_key                  = var.AWS_ACCESS_KEY_ID
+  secret_key                  = var.AWS_SECRET_ACCESS_KEY
+  skip_metadata_api_check     = true
+}
+
+# Create a VPC
+resource "aws_vpc" "authorization_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "authorization-vpc"
+  }
+}
+
+# Create public subnets with valid availability zones for eu-west-1
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id            = aws_vpc.authorization_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "eu-west-1a"
+
+  tags = {
+    Name = "public-subnet-1"
+  }
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id            = aws_vpc.authorization_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "eu-west-1b"
+
+  tags = {
+    Name = "public-subnet-2"
+  }
+}
+
+# Create a security group
+resource "aws_security_group" "authorization_sg" {
+  vpc_id = aws_vpc.authorization_vpc.id
+
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow all incoming traffic on port 8000
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # Allow all outgoing traffic
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "authorization-sg"
+  }
+}
+
+# Create IAM role for ECS task execution
+resource "aws_iam_role" "ecs_execution_role" {
+  name = "ecs_execution_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_policy_attachment" "ecs_execution_policy" {
+  name       = "ecs_execution_policy_attachment"
+  roles      = [aws_iam_role.ecs_execution_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
 resource "aws_dynamodb_table" "users-table" {
@@ -68,11 +143,63 @@ resource "aws_dynamodb_table" "users-table" {
   }
 
   tags = {
-    Name        = "users_table"
+    Name = "users_table"
   }
 }
 
+resource "aws_ecr_repository" "authorization" {
+  name = "authorization"
+}
 
-resource "aws_ecr_repository" "api_image_repository"{
-  name = "api-image"
+resource "aws_ecs_cluster" "authorization" {
+  name = "authorization-cluster"
+}
+
+resource "aws_ecs_task_definition" "authorization" {
+  family                   = "authorization-task"
+  requires_compatibilities = ["FARGATE"]
+  network_mode            = "awsvpc"
+  cpu                     = "256"
+  memory                  = "512"
+
+  execution_role_arn = aws_iam_role.ecs_execution_role.arn # Add execution role
+
+  container_definitions = jsonencode([
+    {
+      name      = "authorization"
+      image     = "${aws_ecr_repository.authorization.repository_url}:latest"
+      memory    = 512
+      cpu       = 256
+      essential = true
+      portMappings = [
+        {
+          containerPort = 8000
+          hostPort      = 8000
+          protocol      = "tcp"
+        }
+      ]
+      environment = [
+        {
+          name  = "AWS_ACCESS_KEY_ID"
+          value = var.AWS_ACCESS_KEY_ID
+        },
+        {
+          name  = "AWS_SECRET_ACCESS_KEY"
+          value = var.AWS_SECRET_ACCESS_KEY
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_ecs_service" "authorization" {
+  name            = "authorization-service"
+  cluster         = aws_ecs_cluster.authorization.id
+  task_definition = aws_ecs_task_definition.authorization.id
+  desired_count   = 1
+
+  network_configuration {
+    subnets          = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
+    security_groups  = [aws_security_group.authorization_sg.id]
+  }
 }
