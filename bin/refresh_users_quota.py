ec0d68242798c8d4e5beab2034575cfff2b2c7df
Nelson Almeida <nelson.martinsalmeidaa@gmail.com>
2024-11-05 22:33:41 +0000

    Added Refresh users query cron job
    Signed-off-by: Nelson Almeida <nelson.martinsalmeidaa@gmail.com>

--- bin/refresh_users_quota.py
+++ bin/refresh_users_quota.py
@@ -7,9 +7,9 @@ load_dotenv()
 
 boto3_client = boto3.client(
     "dynamodb",
-    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-    region_name=os.getenv("AWS_REGION"),
+    aws_access_key_id=f"{os.getenv("AWS_ACCESS_KEY_ID")}",
+    aws_secret_access_key=f"{os.getenv("AWS_SECRET_ACCESS_KEY")}",
+    region_name=f"{os.getenv("AWS_REGION")}",
 )
 
 
